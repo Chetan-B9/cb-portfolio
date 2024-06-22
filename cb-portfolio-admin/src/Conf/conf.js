2a007fe6a9c6120f0f705485228277792e7970ab
Chetan Bedakihale <152062682+Chetan-B9@users.noreply.github.com>
2024-06-22 15:16:59 +0530

    Delete cb-portfolio-admin directory

--- cb-portfolio-admin/src/Conf/conf.js
+++
@@ -1,8 +0,0 @@
-export const Conf = {
-   appWriteEndpoint: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
-   appWriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-   appWriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-   apoWriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-   appWriteThumbnailsBucketId: String(import.meta.env.VITE_APPWRITE_THAMBNAILS_BUCKET_ID),
-   appWriteScreenshotsBucketId: String(import.meta.env.VITE_APPWRITE_SCREENSHOTS_BUCKET_ID)
-}
