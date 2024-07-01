ad8735d7d6319bc74dd565eaafffc8d023f77698
Chetan Bedakihale <chetan.bedakihale80@gmail.com>
2024-07-01 23:24:47 +0530

    Fix test: 24

--- cb-portfolio/src/Components/Home/Home.jsx
+++ cb-portfolio/src/Components/Home/Home.jsx
@@ -92,10 +92,10 @@ function Home() {
     <>
       {/*  hero section start */}
       <section
-        className={` px-8 py-5 md:py-10 md:px-20 lg:px-40  flex flex-col `}
+        className={` px-8 py-5 md:py-10 md:px-20 lg:px-40  flex flex-col overflow-x-hidden`}
       >
         
-        <div className="container h-fit lg:h-[calc(100vh-12rem)] grid grid-cols-1 lg:grid-cols-2 gap-5 md:gap-8">
+        <div className="h-fit lg:h-[calc(100vh-12rem)] grid grid-cols-1 lg:grid-cols-2 gap-5 md:gap-8 w-full max-w-full overflow-x-hidden">
           <div
             className={`${homeStyle.image_part} h-full flex justify-center items-center`}
           >
