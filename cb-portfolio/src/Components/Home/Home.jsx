ce8edde46a94bd90915aa7b1b8752902ae16add1
Chetan Bedakihale <chetan.bedakihale80@gmail.com>
2024-07-01 22:31:13 +0530

    Fix test: 9

--- cb-portfolio/src/Components/Home/Home.jsx
+++ cb-portfolio/src/Components/Home/Home.jsx
@@ -94,7 +94,7 @@ function Home() {
       <section
         className={` px-8 py-5 md:py-10 md:px-20 lg:px-40  flex flex-col `}
       >
-        <div className="container h-fit lg:h-[calc(100vh-12rem)] grid grid-cols-1 lg:grid-cols-2 gap-5 md:gap-8">
+        <div className=" h-fit lg:h-[calc(100vh-12rem)] grid grid-cols-1 lg:grid-cols-2 gap-5 md:gap-8">
           <p>hello</p>
           {/* <div
             className={`${homeStyle.image_part} h-full flex justify-center items-center`}
