522c461c82b47251bbcef26f7696b735e4714780
Chetan Bedakihale <chetan.bedakihale80@gmail.com>
2024-07-04 16:29:39 +0530

    fix issue - 34

--- cb-portfolio/src/Components/Home/Home.jsx
+++ cb-portfolio/src/Components/Home/Home.jsx
@@ -92,7 +92,7 @@ function Home() {
     <>
       {/*  hero section start */}
       <section
-        className={` px-8 py-5 md:py-10 md:px-20 lg:px-40  flex flex-col `}
+        className="px-8 py-5 md:py-10 md:px-20 lg:px-40 flex flex-col"
       >
         
         <div className="container w-full h-fit lg:h-[calc(100vh-12rem)] grid grid-cols-1 lg:grid-cols-2 gap-5 md:gap-8">
